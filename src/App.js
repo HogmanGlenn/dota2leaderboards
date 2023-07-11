c00a8822eae8157bffb2bba136bbf258dd13acb6
LRueckert <lars.rueckert@data4life.care>
2023-07-11 10:18:49 +0200

    fix dom issues and auto format

--- src/App.js
+++ src/App.js
@@ -1,31 +1,25 @@
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import * as React from 'react';
-import './App.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import * as React from "react";
+import "./App.css";
 import Header from "./components/header/Header";
 import Leaderboard from "./components/leaderboard/Leaderboard";
-import Navigation from "./components/navigation/Navigation"
-import Container from '@mui/material/Container';
-import { useState } from 'react';
+import Navigation from "./components/navigation/Navigation";
+import Container from "@mui/material/Container";
+import { useState } from "react";
 
-
-export default function App(){
-
+export default function App() {
   const [filteredPlayers, setFilteredPlayers] = useState([]);
-
 
   return (
     <div className="App">
       <Header />
-      <Container maxWidth="lg" disableScrollLock={true}>
-        <Leaderboard filteredPlayers={filteredPlayers}/>
-        <Navigation setFilteredPlayers={setFilteredPlayers}/>
+      <Container maxWidth="lg">
+        <Leaderboard filteredPlayers={filteredPlayers} />
+        <Navigation setFilteredPlayers={setFilteredPlayers} />
       </Container>
     </div>
   );
 }
-
-
-
--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,28 +1,26 @@
-import * as React from 'react';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import europeData from '../../data/europe/v0001.json';
-import FlagIcon from '@mui/icons-material/Flag';
-import './Navigation.css';
-import { createData } from '../leaderboard/Leaderboard';
+import * as React from "react";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import europeData from "../../data/europe/v0001.json";
+import FlagIcon from "@mui/icons-material/Flag";
+import "./Navigation.css";
+import { createData } from "../leaderboard/Leaderboard";
 
 function getFlagEmoji(countryCode) {
-  if (!countryCode) return '';
+  if (!countryCode) return "";
 
   const codePoints = countryCode
     .toUpperCase()
-    .split('')
-    .map(char => 127397 + char.charCodeAt());
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
 
-    return String.fromCodePoint(...codePoints);
+  return String.fromCodePoint(...codePoints);
 }
 
 function getCountryName(countryCode) {
-
-    const regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
-    return regionNames.of(countryCode) + " " + getFlagEmoji(countryCode);
-
+  const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
+  return regionNames.of(countryCode) + " " + getFlagEmoji(countryCode);
 }
 
 const ITEM_HEIGHT = 48;
@@ -30,7 +28,7 @@ const ITEM_HEIGHT = 48;
 export default function Navigation({ setFilteredPlayers }) {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-  const [selectedCountry, setSelectedCountry] = React.useState('');
+  const [selectedCountry, setSelectedCountry] = React.useState("");
   const handleClick = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -43,18 +41,22 @@ export default function Navigation({ setFilteredPlayers }) {
     setFilteredPlayers(updatedFilteredPlayers);
   };
 
-    React.useEffect(() => {
+  React.useEffect(() => {
     // Update filtered players based on the selected country
-    const updatedFilteredPlayers = createData(europeData.leaderboard).filter((player) => player.countryCode === selectedCountry);
+    const updatedFilteredPlayers = createData(europeData.leaderboard).filter(
+      (player) => player.countryCode === selectedCountry
+    );
     setFilteredPlayers(updatedFilteredPlayers);
   }, [selectedCountry]);
 
-  const filteredPlayers = createData(europeData.leaderboard).filter(player => europeData.leaderboard.some(entry => entry.country === player.countryCode));
+  const filteredPlayers = createData(europeData.leaderboard).filter((player) =>
+    europeData.leaderboard.some((entry) => entry.country === player.countryCode)
+  );
 
   // Sort the players array by countryCode in alphabetical order
   filteredPlayers.sort((a, b) => {
-    const countryCodeA = (a.countryCode || '').toUpperCase();
-    const countryCodeB = (b.countryCode || '').toUpperCase();
+    const countryCodeA = (a.countryCode || "").toUpperCase();
+    const countryCodeB = (b.countryCode || "").toUpperCase();
 
     if (countryCodeA < countryCodeB) {
       return -1;
@@ -72,54 +74,50 @@ export default function Navigation({ setFilteredPlayers }) {
         className="long-menu"
         aria-label="more"
         id="long-button"
-        aria-controls={open ? 'long-menu' : undefined}
-        aria-expanded={open ? 'true' : undefined}
+        aria-controls={open ? "long-menu" : undefined}
+        aria-expanded={open ? "true" : undefined}
         aria-haspopup="true"
         color="inherit"
-        
         onClick={handleClick}
-        style={{fontSize: '32px'}}
+        style={{ fontSize: "32px" }}
       >
-        <FlagIcon fontSize='large'/>
+        <FlagIcon fontSize="large" />
       </IconButton>
-      <Menu classname="menu-country"
+      <Menu
+        className="menu-country"
         id="long-menu"
         MenuListProps={{
-          'aria-labelledby': 'long-button',
+          "aria-labelledby": "long-button",
         }}
         anchorEl={anchorEl}
         open={open}
         onClose={handleClose}
-        anchorOrigin={{vertical: 'bottom', horizontal: 'center'}}
-        transformOrigin={{vertical: 'top', horizontal: 'center'}}
-        disableScrollLock={true}     
+        anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        transformOrigin={{ vertical: "top", horizontal: "center" }}
+        disableScrollLock={true}
         PaperProps={{
           style: {
             maxHeight: ITEM_HEIGHT * 5.5,
-            width: '30ch',
+            width: "30ch",
           },
         }}
-      > 
-
+      >
         {filteredPlayers.map((countries) => {
-        if (temp.includes(countries.countryCode)) {
-          return null;
-        }
-        else {
-          temp.push(countries.countryCode);
-          return (
-            <MenuItem
+          if (temp.includes(countries.countryCode)) {
+            return null;
+          } else {
+            temp.push(countries.countryCode);
+            return (
+              <MenuItem
                 key={countries.countryCode}
                 onClick={() => handleClose(countries.countryCode)}
-            >
+              >
                 {getCountryName(countries.countryCode.toUpperCase())}
-            </MenuItem>
-          );
-        }
+              </MenuItem>
+            );
+          }
         })}
       </Menu>
     </div>
   );
 }
-
-  
